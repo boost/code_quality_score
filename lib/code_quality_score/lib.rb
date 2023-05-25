1aeef26dd4c859bdfe905314e10996602fdb8f00
Isabel Anastasiadis <isabel@boost.co.nz>
2023-05-26 11:13:45 +1200

    fix: actually fixed the is_gem and also the expected directory to run against

--- lib/code_quality_score/lib.rb
+++ lib/code_quality_score/lib.rb
@@ -9,18 +9,18 @@ def app_folder_file_count(solution)
 end
 
 def structural_similarity_score_per_file(solution)
-  score_line = `flay #{solution[:relative_path]}#{solution[:name]}/#{folder(solution)}/* | head -n 1`
+  score_line = `flay #{solution[:relative_path]}#{folder(solution)}/* | head -n 1`
   score_number = Float(score_line.split(" ").last)
   (score_number / app_folder_file_count(solution)).round(2)
 end
 
 def abc_method_average_score(solution)
-  score_line = `flog #{solution[:relative_path]}#{solution[:name]}/#{folder(solution)}/* | head -n 2 | tail -1`
+  score_line = `flog #{solution[:relative_path]}#{folder(solution)}/* | head -n 2 | tail -1`
   Float(score_line.split(":").first)
 end
 
 def code_smells_per_file(solution)
-  score_line = `reek #{solution[:relative_path]}#{solution[:name]}/#{folder(solution)}/* | tail -1`
+  score_line = `reek #{solution[:relative_path]}#{folder(solution)}/* | tail -1`
   score_number = Float(score_line.split(" ").first)
   (score_number / app_folder_file_count(solution)).round(2)
 end
