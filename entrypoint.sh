cf71d6c9a8b7a1a346338e8b216dd37c99cbbfff
Isabel Anastasiadis <isabel@boost.co.nz>
2023-05-26 13:35:11 +1200

    feat: started getting the action to have the right interface with stubbed data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
 echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+
+echo "abc_method_average=6.4" >> $GITHUB_OUTPUT
+echo "code_smells_per_file=0.5" >> $GITHUB_OUTPUT
+echo "similarity_score_per_file=12.0" >> $GITHUB_OUTPUT
+echo "total_score=18.9" >> $GITHUB_OUTPUT
