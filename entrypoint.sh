7d3375304a120395d5f59a503fe4cb9181f31b7f
Isabel Anastasiadis <isabel@boost.co.nz>
2023-05-26 14:43:01 +1200

    fix: trying to get output actually working

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 echo "Hello $1"
 
-echo "abc_method_average=6.4" >> $GITHUB_OUTPUT
-echo "code_smells_per_file=0.5" >> $GITHUB_OUTPUT
-echo "similarity_score_per_file=12.0" >> $GITHUB_OUTPUT
-echo "total_score=18.9" >> $GITHUB_OUTPUT
+echo “::set-output name=total_score::18.9”
+
+# echo "abc_method_average=6.4" >> $GITHUB_OUTPUT
+# echo "code_smells_per_file=0.5" >> $GITHUB_OUTPUT
+# echo "similarity_score_per_file=12.0" >> $GITHUB_OUTPUT
+# echo "total_score=18.9" >> $GITHUB_OUTPUT
