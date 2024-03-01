c1261e66cdac5f873f158ac6f9056941fcfae499
Andy Anastasiadis-Gray <andy@boost.co.nz>
2024-03-01 16:24:00 +1300

    ci: testing bash script for generating diffs

--- lib/code_quality_score/score_snapshot.rb
+++ lib/code_quality_score/score_snapshot.rb
@@ -47,21 +47,21 @@ module CodeQualityScore
     end
 
     def structural_similarity_score(folders)
-      score_line = `flay #{folders}/* | head -n 1`
+      score_line = `flay #{folders} | head -n 1`
       score_number = Float(score_line.split(" ").last).round(2)
       weighted_score = score_number * @score_weights[:similarity_score]
       weighted_score.round(2)
     end
 
     def abc_method_average_score(folders)
-      score_line = `flog #{folders}/* | head -n 2 | tail -1`
+      score_line = `flog #{folders} | head -n 2 | tail -1`
       score = Float(score_line.split(":").first).round(2)
       weighted_score = score * @score_weights[:abc_method_average]
       weighted_score.round(2)
     end
 
     def code_smells_per_file(folders, file_count)
-      score_line = `reek #{folders}/* | tail -1`
+      score_line = `reek #{folders} | tail -1`
       score_number = Float(score_line.split(" ").first)
       score_per_file = (score_number / file_count).round(2)
       weighted_score = score_per_file * @score_weights[:code_smells_per_file]
